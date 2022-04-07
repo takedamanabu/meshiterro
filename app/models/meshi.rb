3a80683d211ee9662658ad1ef34ae3845e2ba13e
EC2 Default User <ec2-user@ip-172-31-26-21.ec2.internal>
2022-04-07 14:02:55 +0000

    meshiterro First

---
+++ app/models/meshi.rb
@@ -0,0 +1,2 @@
+class Meshi < ApplicationRecord
+end
---
+++ db/migrate/20220407133446_create_meshis.rb
@@ -0,0 +1,10 @@
+class CreateMeshis < ActiveRecord::Migration[6.1]
+  def change
+    create_table :meshis do |t|
+      t.string :title
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
